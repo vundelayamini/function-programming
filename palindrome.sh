15f553b6a12d55a0e2b4f36a237a0e73915a8f1f
yamini <vundelayamini@gmail.com>
2021-02-09 23:36:07 -0800

    Added:

--- palindrome.sh
+++ palindrome.sh
@@ -2,21 +2,21 @@
 
 function myFunc() {
 read -p "enter the number:"num
- local num=$num
-     local num2 =0
-      while [ $num -ne 0]
+     digit=$digit
+     rev=0
+     n=num
+      while [ $digit -ne 0]
       do
-         n=$(($num%10))
-         num2=$(($num2*10)+$n))
-          number=$(($num/10))
+         digit=$((num%10))
+         rev=$( (rev*10)+digit) )
+          num=$((num/10))
      done
-     echo $num2
-}
-  if [ $number -eq  0 ]
+     echo $rev
+
+  if [ $digit -eq  $rev ]
      then
            echo "  number is palindrome "
        else
             echo "  number is not palindrome"
       fi
-
-palindrome=$( palindromeNumber $num )
+}
