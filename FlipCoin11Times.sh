18a3c4ef7a74493264c152e261e60120e57ba8e1
yamini <vundelayamini@gmail.com>
2021-02-17 18:50:48 +0530

    Added : example for FlipCoin11tTimes

---
+++ FlipCoin11Times.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+Head=1
+Tail=1
+
+while [[ $Head -le 11 ]] && [[ $Tail -le 11 ]]
+do
+		value=$((RANDOM%2))
+
+		if [ $value -eq 0 ]
+		then
+				((Head++))
+		else
+				((Tail++))
+		fi
+done
+
+
+if [ $Head -eq 12 ]
+then
+     echo Head Wins 11 Times
+else
+     echo Tails Wins 11 Times
+fi
